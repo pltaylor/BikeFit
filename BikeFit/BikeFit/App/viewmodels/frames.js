b37d400640f3df057ab59860a40995e21240739d
Perry Taylor <ptaylor@rcrracing.com>
2013-04-24 19:54:13 -0400

    populating manufacturers dropdown

--- BikeFit/BikeFit/App/viewmodels/frames.js
+++ BikeFit/BikeFit/App/viewmodels/frames.js
@@ -1,6 +1,18 @@
-﻿define(['services/logger'], function (logger) {
+﻿define(['services/logger',
+        "services/datacontext"],
+    function (logger, datacontext) {
+    var manufacturers = ko.observableArray();
+
+    var frame1 = ko.observable(true);
+    var frame2 = ko.observable(true);
+    var frame3 = ko.observable(true);
+
     var vm = {
         activate: activate,
+        frame1: frame1,
+        frame2: frame2,
+        frame3: frame3,
+        manufacturers: manufacturers,
         title: 'Frames'
     };
 
@@ -8,6 +20,7 @@
 
     //#region Internal Methods
     function activate() {
+        datacontext.getManufacturers(manufacturers);
         logger.log('Frames View Activated', null, 'frames', true);
         return true;
     }
